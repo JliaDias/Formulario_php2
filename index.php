d82ed707f7a454f278410f9c8cae5b358a4d0bc6
JliaDias <julia.goncalves83@etec.sp.gov.br>
2022-05-05 13:29:13 -0300

    Arquivos do programa

---
+++ index.php
@@ -0,0 +1,33 @@
+<DOCTYPE html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+        <form action="controller/controllerPessoa.php" method="post">
+            <table>
+                <tr>
+                    <td>
+                        <label for="lblNome">Nome:</label>
+                    </td>
+                    <td>
+                        <input type="text" name="txtNome" id="txtNome">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="lblEnd">Endereço:</label>
+                    </td>
+                    <td>
+                        <input type="text" name="txtEnd" id="txtEnd">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button>Cadastrar</button>
+                    </td>
+                </tr>               
+            </table>
+        </form>
+    </body>
+</html>
